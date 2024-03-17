7163402083bf3490be98f5bf92d3588932aad457
Jay Gupta <146335736+jaygupta17@users.noreply.github.com>
2024-03-17 12:37:12 +0000

    Color change : 01app

--- 01app/01app/src/App.js
+++ 01app/01app/src/App.js
@@ -1,8 +1,15 @@
+import { useState } from "react";
 function App() {
+  
+  let [color, setcolor] = useState("cyan");
   return (
-    <div className="App">
-     
-    </div>
+    <>
+      <div className="main h-screen flex justify-center items-center gap-x-3 duration-200" style={{backgroundColor : color}}>
+        <button className="py-4 px-7 bg-yellow-500 flex justify-center items-center border-solid border-2 border-sky-200" onClick={()=>{setcolor('yellow')}}> Yellow </button>
+        <button className="py-4 px-7 bg-blue-500 flex justify-center items-center border-solid border-2 border-sky-200" onClick={()=>{setcolor('blue')}}> Blue </button>
+        <button className="py-4 px-7 bg-red-500 flex justify-center items-center border-solid border-2 border-sky-200" onClick={()=>{setcolor('red')}}> Red </button>
+      </div>
+    </>
   );
 }
 
