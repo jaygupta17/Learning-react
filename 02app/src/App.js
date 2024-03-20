0ce09c42740378be7800254507e5d44eed0d9115
Jay Gupta <146335736+jaygupta17@users.noreply.github.com>
2024-03-20 15:28:37 +0000

    02app : password generator completed

--- 02app/src/App.js
+++ 02app/src/App.js
@@ -1,20 +1,45 @@
-import { useState } from "react";
+import { useState, useCallback , useRef , useEffect} from "react";
 function App() {
+
   const [length, setLength] = useState(8);
   const [numAllowed, setNumAllowed] = useState(false);
   const [charAllowed, setcharAllowed] = useState(false);
   const [password, setpassword] = useState("");
-  let generate = ()=>{
-    
+
+  let generate = useCallback(()=>{
+
+    let pass = "";
+    let str= "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    if(numAllowed) str+= "0123456789"
+    if(charAllowed) str+= "!@#$%^&*()_+"
+
+    for (let i = 0; i < length; i++) {
+      const char =Math.floor(Math.random() * str.length + 1);
+      pass += str.charAt(char) 
+    }
+    setpassword(pass)
+
+  } , [length , numAllowed , charAllowed]);
+
+  useEffect(()=>{
+    generate();
+  } ,[length , numAllowed , charAllowed])
+
+  let copyPassw = (e)=>{
+    window.navigator.clipboard.writeText(password);
+    passwordRef.current.select(); 
   }
+
+  const passwordRef = useRef(null);
+
   return (
     <>
       <div className="bg-gray-500 h-screen w-full flex justify-center items-center">
         <div className="h-1/2 w-3/5 flex justify-center items-center flex-col rounded-lg ">
           <h1 className="text-xl text-white font-semibold">Password Generator</h1>
           <div className="flex justify-center items-center flex-row">
-            <input type="text" value={password} readOnly placeholder="Password" className="outline-none w-full py-1 px-3 rounded"/>
-            <button className="py-1 px-7 bg-blue-500 flex justify-center items-center border-solid border-2 border-sky-200 rounded"> Copy </button>
+            <input type="text" ref={passwordRef} value={password} readOnly placeholder="Password" className="outline-none w-full py-1 px-3 rounded"/>
+            <button className="py-1 px-7 bg-blue-500 flex justify-center items-center border-solid border-2 border-sky-200 rounded" onClick={(e)=>copyPassw(e)}> Copy </button>
           </div>
           <div className="flex justify-center items-center flex-row">
             <input type="range" name="length" min={4} max={50} value={length} className="cursor-pointer" onChange={(e)=>{setLength(e.target.value)}}/>
