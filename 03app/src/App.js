ed1b9faedcd902840d5091e3228d3116f7892de7
Jay Gupta <146335736+jaygupta17@users.noreply.github.com>
2024-03-21 16:14:35 +0000

    03app : setup done

---
+++ 03app/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="h-screen flex justify-center items-center bg-slate-400">
+      
+    </div>
+  );
+}
+
+export default App;
