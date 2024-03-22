713a68508b723a7691916794e065b48dd4c3ce7d
Jay Gupta <146335736+jaygupta17@users.noreply.github.com>
2024-03-22 11:15:18 +0000

    Config done

--- 03app/src/App.js
+++ 03app/src/App.js
@@ -1,8 +1,8 @@
 import './App.css';
-
+import { BrowserRouter as Router,Routes,Route } from "react-router-dom";
 function App() {
   return (
-    <div className="h-screen flex justify-center items-center bg-slate-400">
+    <div className="h-screen flex justify-center items-center bg-blue-400">
       
     </div>
   );
