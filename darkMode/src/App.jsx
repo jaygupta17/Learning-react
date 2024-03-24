88210d15465733e7349c0ff388614f5c265f9580
Jay Gupta <146335736+jaygupta17@users.noreply.github.com>
2024-03-24 14:20:57 +0000

    06 : dark mode setup done

---
+++ darkMode/src/App.jsx
@@ -0,0 +1,12 @@
+import Card from "./Components/Card"
+
+function App() {
+  return (
+    <div className="bg-neutral-500 h-screen flex justify-center items-center flex-col">
+      <button>Toggle</button>
+      <Card/>
+    </div>
+  )
+}
+
+export default App
---
+++ darkMode/src/Components/Card.jsx
@@ -0,0 +1,9 @@
+function Card() {
+  return (
+    <div className="h-1/2 w-1/4 bg-white flex justify-center items-center">
+        <h1>Hello World</h1>
+    </div>
+  )
+}
+
+export default Card
