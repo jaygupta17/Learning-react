7a3fa10ef77ea1f3d441f910debd0de8d0133ea2
Jay Gupta <146335736+jaygupta17@users.noreply.github.com>
2024-03-24 04:41:51 +0000

    04: setup

---
+++ 04ReactRouter/src/Components/index.js
@@ -0,0 +1,6 @@
+import { Footer } from "../Components/Footer";
+import { Home } from "../Components/Home";
+import { About } from "../Components/About";
+import { Header } from "../Components/Header";
+
+export {Footer , Home , About , Header}
